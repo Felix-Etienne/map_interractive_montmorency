fed201bad79a2cfdaac3cdb7fa0a45d033f2913b
Félix-Etienne Hamel <fe.hamel06@gmail.com>
2025-10-07 13:46:01 -0400

    Premiere et deuxieme étage bloc L fini

--- frontend/src/components/Map/Map.jsx
+++ frontend/src/components/Map/Map.jsx
@@ -1,31 +1,17 @@
 import "./Map.css";
-import image from "../../Images/a7fc6ee5.png";
-import { useEffect } from "react";
+import Premier from "./svgFiles/bloc-L/premier.jsx";
+import Deuxieme from "./svgFiles/bloc-L/deuxieme.jsx";
+import { useEffect, useState } from "react";
 import SearchBar from "../SearchBar/SearchBar";
 
-export default function Map({ imageSize = [2000, 1000], imageUrl = image, polygons = [] }) {
+export default function Map() {
 
-    useEffect(() => {
-        document.querySelectorAll(".salle").forEach(salle => {
-            salle.addEventListener("click", () => {
-                const id = salle.id;
-                console.log("Salle cliquée :", id);
+    const [level, setLevel] = useState("1");
 
-                highlightClass(id);
-            });
-        });
-
-        function highlightClass(id) {
-            document.querySelectorAll(".salle").forEach(e => {
-                e.setAttribute("fill", "lightblue");
-            });
-
-            if (!id) return;
-
-            let el = document.getElementById(id);
-            if (el) el.setAttribute("fill", "lightgreen");
-        }
-    }, []);
+    const maps = {
+        1: <Premier />,
+        2: <Deuxieme />,
+    };
 
 
 
@@ -35,33 +21,16 @@ export default function Map({ imageSize = [2000, 1000], imageUrl = image, polygo
             <div className="map-container">
                 <SearchBar />
                 <p>Voici la Map interactive du college Montmorency : </p>
+                <select
+                    value={level}
+                    onChange={(e) => setLevel(e.target.value)}
+                >
+                    <option value="1">1er Étage</option>
+                    <option value="2">2em Étage</option>
+                </select>
+
                 <div>
-                    <svg id="plan" viewBox="0 0 1000 600" width="100%">
-                        <image href={image} x="0" y="0" width="100%" height="600" />
-                        <g >
-                            <rect id="L1760" class="salle" x="455" y="10" width="105" height="135" fill="lightblue" stroke="black" />
-                            <text x="510" y="85" fontSize="20" textAnchor="middle" fill="black">L-1760</text>
-                        </g>
-                        <g>
-                            <rect id="L1762" class="salle" x="345" y="10" width="110" height="135" fill="lightblue" stroke="black" />
-                            <text x="400" y="85" fontSize="20" textAnchor="middle" fill="black">L-1762</text>
-                        </g>
-                        <g>
-                            <rect id="L1758" class="salle" x="560" y="10" width="105" height="135" fill="lightblue" stroke="black" />
-                            <text x="610" y="85" fontSize="20" textAnchor="middle" fill="black">L-1758</text>
-                        </g>
-                        <g>
-                            <rect id="L1756" class="salle" x="560" y="190" width="105" height="140" fill="lightblue" stroke="black" />
-                            <text x="610" y="265" fontSize="20" textAnchor="middle" fill="black">L-1756</text>
-                        </g>
-                        <g>
-                            <rect id="C1" class="" x="510" y="190" width="40" height="310" fill="lightblue" stroke="black" />
-                        </g>
-                        <g>
-                            <rect id="C1" class="" x="346" y="145" width="320" height="45" fill="lightblue" stroke="black" />
-                        </g>
-                        <rect id="sortie" class="exit" x="504" y="500" width="50" height="40" fill="orange" stroke="black" />
-                    </svg>
+                    <div className="map">{maps[level]}</div>
                 </div>
             </div >
         </>
