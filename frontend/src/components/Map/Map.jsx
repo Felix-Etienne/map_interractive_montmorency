da3c81d287f18474070d35f1f011368c41978ac7
Félix-Etienne Hamel <fe.hamel06@gmail.com>
2025-10-07 15:18:01 -0400

    svg Interactif

--- frontend/src/components/Map/Map.jsx
+++ frontend/src/components/Map/Map.jsx
@@ -1,19 +1,18 @@
 import "./Map.css";
 import Premier from "./svgFiles/bloc-L/premier.jsx";
 import Deuxieme from "./svgFiles/bloc-L/deuxieme.jsx";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import SearchBar from "../SearchBar/SearchBar";
+
+
 
 export default function Map() {
 
     const [level, setLevel] = useState("1");
-
     const maps = {
         1: <Premier />,
         2: <Deuxieme />,
     };
-
-
 
 
     return (
@@ -28,7 +27,6 @@ export default function Map() {
                     <option value="1">1er Étage</option>
                     <option value="2">2em Étage</option>
                 </select>
-
                 <div>
                     <div className="map">{maps[level]}</div>
                 </div>
