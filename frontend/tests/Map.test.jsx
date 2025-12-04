09b2f81655e24afdaa65ff4865dfc679453284e5
Adam Mahmoudi <2396547@cmontmorency.qc.ca>
2025-12-04 16:22:18 -0500

    Setup Vitest

---
+++ frontend/tests/Map.test.jsx
@@ -0,0 +1,10 @@
+ import {render, screen} from '@testing-library/react'
+ import userEvent from'@testing-library/user-event'
+import { describe, expect, it } from 'vitest';
+
+describe('Map', () => {
+    it('isShown', () => {
+        render(<Map/>);
+        //Test HERE
+    })
+})
